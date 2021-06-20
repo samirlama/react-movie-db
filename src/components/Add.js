cccc060f54a5ed95d716d14631df160a4b25e1dd
Sameer <samirlama509@gmail.com>
2021-06-20 19:51:22 +0545

    watchlist component created.

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResultCard from './ResultCard';
+
 const Add = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function requestApi(e){
+    setQuery(e.target.value);
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&query=${e.target.value}`)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    setResults(data.results);
+  };
+
   return (
-    <h1>Add Page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input type="text"
+                   placeholder="Search for a movie"
+                   value={query}
+                   onChange={requestApi}
+            />
+            { results.length > 0 && (
+              <ul className="results">
+                {
+                  results.map((res) => {
+                    return (
+                      <li key={res.id}>
+                        <ResultCard movie={res} />
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
